158438ab425b8514b52c1a8d3f533f0c6e698822
janrae-l <105533574+janrae-l@users.noreply.github.com>
2023-11-28 13:25:38 +0800

    Add DOM methods for page functionality
    The DOM methods makes the page interactive and adds functionality. Through manipulating the different CSS and HTML elements, it adds life to the page as the user plays the game

--- guessing-game/script.js
+++ guessing-game/script.js
@@ -2,20 +2,48 @@
 
 let score = 20;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let highscore = 0;
 
-document.querySelector(".number").textContent = secretNumber;
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
 
-document.querySelector(".check").addEventListener('click', function () {
-    if 
+  if (!guess) {
+    document.querySelector("message").textContent = "No number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector(".message").textContent =
+      "You won! Congratulations!";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "25rem";
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent =
+        guess > secretNumber ? "Too high" : "Too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent =
+        "No score left. You lose!";
+      document.querySelector("body").style.backgroundColor = "#ff0000";
+      document.querySelector(".number").textContent = secretNumber;
+    }
+  }
 });
 
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector(".score").value = score;
+  document.querySelector(".score").textContent = score;
   document.querySelector(".message").textContent = "Start guessing...";
   document.querySelector("body").style.backgroundColor = "#333";
+  document.querySelector(".number").style.width = "13rem";
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").textContent = "";
 });
